b8b49cfe002b7fea7f3ce54a35484779b8de977d
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-22 11:34:12 +0900

    Up to Class Elevator : Done

--- Commercial_Controller/column.cs
+++ Commercial_Controller/column.cs
@@ -26,16 +26,107 @@ namespace Commercial_Controller
             this.callButtonsList = new List<string>();
             this.servedFloorsList = _servedFloors;
             this.isBasement = _isBasement;
-            // this.createElevators( _amountOfFloors, _amountOfElevators);
-            // this.createCallButton(_amountOfFloors, _isBasement);
+            // this.createElevators(_amountOfFloors, _amountOfElevators);
+            // this.createCallButtons(_amountOfFloors, _isBasement);
             // this.requestElevator(int userPosition, string direction)
         }
 
-        //Simulate when a user press a button on a floor to go back to the first floor
+        // public createCallButtons(_amountOfFloors, _isBasement)
+        // {
+
+        // }
+
+
+        // public createElevators(_amountOfFloors, _amountOfElevators)
+
+
+        
+        // Simulate when a user press a button on a floor to go back to the first floor
         // public Elevator requestElevator(int userPosition, string direction)
         // {
             
         // }
 
+        public Elevator findElevator(int requestedFloor, string requestedDirection)
+        {
+            BestElevatorInformations bestElevator;
+            int bestScore = 6;
+            int referenceGap = 10000000;
+            BestElevatorInformations bestElevatorInformations;
+
+            if (requestedFloor == 1)
+            {
+                foreach (string elevator in this.elevatorsList) 
+                    // The elevator is at the lobby and already has some requests. It is about to leave but has not yet departed.
+                    if (1 == elevator.currentFloor && elevator.status == "stopped")
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(1, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                     // The elevator is at the lobby and has no requests
+                    else if (1 == elevator.currentFloor && elevator.status == "idle")
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(2, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                    // The elevator is lower than me and is coming up. It means that I'm requesting an elevator to go to a basement, and the elevator is on it's way to me.
+                    else if (1 > elevator.currentFloor && elevator == "up")
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(3, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                    // The elevator is above me and is coming down. It means that I'm requesting an elevator to go to a floor, and the elevator is on it's way to me
+                    else if (1 < elevator.currentFloor && elevator.direction == "down")
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(3, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                    // The elevator is not at the first floor, but doesn't have any request
+                    else if (elevator.status == "idle")
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(4, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                    // The elevator is not available, but still could take the call if nothing better is found
+                    else
+                    {
+                        BestElevatorInformations bestElevatorInformations = this.checkIfElevatorIsBetter(5, elevator, bestElevatorInformations, requestedFloor);
+                    }
+                    bestElevator = bestElevatorInformations.bestElevator;
+                    bestScore = bestElevatorInformations.bestScore;
+                    referenceGap = bestElevatorInformations.referenceGap;
+                    }
+            }
+
+        }
+
+
+        public BestElevatorInformations checkIfElevatorIsBetter(int scoreToCheck, string newElevator, BestElevatorInformations bestElevatorInformations, int floor)
+        {            
+            if (scoreToCheck < bestElevatorInformations.bestScore)
+                {
+                    bestElevatorInformations.bestScore = scoreToCheck;
+                    bestElevatorInformations.bestElevator = newElevator;
+                    bestElevatorInformations.referenceGap = Math.Abs(newElevator.currentFloor - floor);
+                }
+            else if (bestElevatorInformations.bestScore == scoreToCheck)
+            {
+                int gap = Math.Abs(newElevator.currentFloor - floor);
+                if (bestElevatorInformations.referenceGap > gap)
+                {
+                    bestElevatorInformations.bestElevator = newElevator;
+                    bestElevatorInformations.referenceGap = gap;
+                }
+            }
+            return bestElevatorInformations;
+        }    
+    }
+    public class BestElevatorInformations
+        {
+        public string bestElevator;
+        public int bestScore;
+        public int referenceGap;
+
+        public BestElevatorInformations(string _bestElevator, int _bestScore, int _referenceGap)
+        {
+            this.bestElevator = _bestElevator;
+            this.bestScore = _bestScore;
+            this.referenceGap = _referenceGap;
+        }
     }
 }
--- Commercial_Controller/elevator.cs
+++ Commercial_Controller/elevator.cs
@@ -13,6 +13,8 @@ namespace Commercial_Controller
         public List<int> floorRequestList;
         public string direction;
         public bool overweight;
+        public bool obstruction;
+        public int screenDisplay;
         public Elevator(int _id, string _status, int _amountOfFloors, int _currentFloor, int _elevatorID)
         {
             this.ID = _id;
@@ -23,15 +25,104 @@ namespace Commercial_Controller
             this.floorRequestList = new List<int>();
             this.direction = null;
             this.overweight = false;
-            {
-                
-            }
-
+            this.obstruction = false;
+            this.screenDisplay = 1;
         }
         public void move()
         {
-
+        while (this.floorRequestList.Count != 0)
+            {
+                int destination = this.floorRequestList[0];
+                this.status = "moving";
+                if (this.currentFloor < destination)
+                {
+                   this.direction = "up";
+                   this.sortFloorList();
+                   while (this.currentFloor < destination)
+                   {
+                    this.currentFloor++;
+                    this.screenDisplay = this.currentFloor;
+                   }
+                }
+                else if (this.currentFloor > destination)
+                {
+                    this.direction = "down";
+                    this.sortFloorList();
+                    while (this.currentFloor < destination)
+                    {
+                        this.currentFloor--;
+                        this.screenDisplay = this.currentFloor;
+                    }
+                }
+                this.status = "stopped";
+                this.operateDoors();
+                this.floorRequestList.RemoveAt(0);                
+            }
+            this.status = "idle";
         }
         
+        public void sortFloorList()
+        {
+            if (this.direction == "up")
+            {
+               this.floorRequestList.Sort();
+            }
+            else
+            {
+                this.floorRequestList.Reverse();
+            }
+        }
+
+        public void operateDoors()
+        {
+            this.door.status = "opened";
+            // Console.WriteLine("Wait 5 seconds");
+            if (this.overweight == false)
+            {
+                this.door.status = "closing";
+                if (this.obstruction == false)
+                {
+                    this.door.status = "closed";
+                }
+                else
+                {
+                    this.operateDoors();
+                }
+            }
+            else
+            {
+                while (this.overweight == true)
+                {
+                    // Console.WriteLine("Activate overweight alarm")
+                }
+                this.operateDoors();
+            }
+        }
+
+        public void addNewRequest(int requestedFloor)
+        {
+        if (this.floorRequestList.Count != requestedFloor)
+            {
+                this.floorRequestList.Add(requestedFloor);
+            }
+        if (this.currentFloor < requestedFloor)
+            {
+                this.direction = "up";
+            }
+        if (this.currentFloor > requestedFloor)
+            {
+                this.direction = "down";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
